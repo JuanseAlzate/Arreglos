58978d2306be1059a6b7137713cea49744497f34
JuanseAlzate <juansebastianalzatelopez251@gmail.com>
2024-09-15 22:03:12 -0500

    ruta de prueba

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/arr")
+async def arr(req: Request):
+    body = await req.json()
+    return body
